fe860f065f80e092f48677d4061069f651da1ea3
daoyi7 <codingwell@gmail.com>
2018-03-03 23:43:47 +0800

    update:indexRouter Redirect

--- src/entries/routes.js
+++ src/entries/routes.js
@@ -1,4 +1,4 @@
-// import React from 'react'
+import React from 'react'
 import Welcome from 'container/welcome'
 import Home from 'container/home'
 import Root from 'container/root'
@@ -8,7 +8,16 @@ const routes = [
     path: '/',
     component: Root,
     indexRoute: { component: Home },
+    onEnter: (nextState, replaceState) => {
+      if (nextState.location.pathname == '/') {
+        replaceState('/index')
+      }
+    },
     childRoutes: [
+      {
+        path: 'index',
+        component: Home
+      },
       {
         path: 'welcome',
         component: Welcome
